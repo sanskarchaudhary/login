9c0186c32373b70bfd1455a2505a77507916f47e
sanskarchaudhary <sanskarchaudhary@gmail.com>
2024-05-11 11:46:38 +0530

    Update

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-document.querySelector('.add-to-cart').addEventListener('submit', function(event) {
-    var quantity = document.querySelector('#qty-2').value;
-    var productName = document.querySelector('input[name="product-name"]').value;
-    var productPrice = document.querySelector('input[name="product-price"]').value;
+var addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-    console.log('Quantity:', quantity);
-    console.log('Product Name:', productName);
-    console.log('Product Price:', productPrice);
-    localStorage.setItem('product',JSON.stringify({quantity , productName , productPrice }))
-    // Now you can use these values in your backend logic
-    // For example, you might send them to your server using an AJAX request
+addToCartButtons.forEach(function(button) {
+    button.addEventListener('submit', function(event) {
+        // Use event.target to get the form that the button is inside of
+        var form = event.target;
+
+        // Use the form to query for the elements inside it
+        var quantity = form.querySelector('.qty').value;
+        var productName = form.querySelector('input[name="product-name"]').value;
+        var productPrice = form.querySelector('input[name="product-price"]').value;
+
+        console.log('Quantity:', quantity);
+        console.log('Product Name:', productName);
+        console.log('Product Price:', productPrice);
+        const product = {quantity , productName , productPrice};
+        let productColl = JSON.parse(localStorage.getItem('product'));
+
+            if (!productColl) {
+                productColl = [];
+            }
+
+            productColl.push(product);
+
+            localStorage.setItem('product', JSON.stringify(productColl));
+        // Now you can use these values in your backend logic
+        // For example, you might send them to your server using an AJAX request
+    });
 });
